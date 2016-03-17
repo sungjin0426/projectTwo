c901e102de1782e1d3f4ff4d4de2a5a7ac46fc6b
sungjin0426 <sjlbest0426@gmail.com>
2016-03-17 19:36:03 -0400

    project Two

--- main.js
+++ main.js
@@ -1,36 +1,51 @@
 window.onload = function() {
   console.log("main.js is fully loaded");
-
+  //initial grab
+  var mapMe = document.querySelector('#map-me');
   var findMeBtn = document.querySelector('.find-me-btn');
   var foundMe = document.querySelector('.current-location');
   var latLong = document.querySelector('.lat-long');
+  var myLabel = document.querySelector('.my-label');
+  var parkSearch = document.querySelector('#park-search');
+  var subBtn = document.querySelector('#submit-btn');
+  var dropdown = document.querySelector('#drop');
 
   var infoContainer = document.getElementById('info-container');
   infoContainer.style.display = 'none';
+  myLabel.style.display = 'none';
+  subBtn.style.display = 'none';
+  parkSearch.style.display = 'none';
   var query = 'https://data.cityofnewyork.us/resource/sxx4-xhzg.json';
 
+  // determine the current position
+  // received direction from Mozilla MDN
   findMeBtn.addEventListener("click", function(event){
-    // determine the current position
-    var success = function (position) {
-      var currentLat = position.coords.latitude;
-      var currentLon = position.coords.longitude;
-      var myCoord = [currentLat, currentLon];
-      console.log(myCoord);
-      APIcall(currentLat, currentLon);
-    };
-    var error = function() {
-      console.log("Unable");
-    };
-    var findSelf = navigator.geolocation.getCurrentPosition(success, error);
-    console.log(findSelf);
-  });
-
-  var APIcall = function(currentLat, currentLon) {
-  console.log('My location is at latitude: ' + currentLat + ", longitude:" + currentLon);
-  findMeBtn.style.diplay = "none";
-  foundMe.innerHTML = "I am at:";
-  latLong.innerHTML = "Lat: " + currentLat + ", Long: " + currentLon;
+  console.log("Find me where i am!");
+  var mapMe = document.querySelector('#map-me');
+  if (!navigator.geolocation){
+    mapMe.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    console.log("Turn on your locator");
   }
+  //if geolocation is successfully loaded,
+  function success(position) {
+    var myLat  = position.coords.latitude;
+    var myLong = position.coords.longitude;
+    mapMe.innerHTML = '<p>Latitude is: ' + myLat + '° <br>Longitude is: ' + myLong + '°</p>';
+    console.log(myLat, myLong);
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + myLat + "," + myLong + "&zoom=18&size=250x250&sensor=false";
+    mapMe.appendChild(img);
+  };
+  //if geolocation is failed to load,
+  function error() {
+    mapMe.innerHTML = "Unable to retrieve your location";
+  };
+    mapMe.innerHTML = "<p>Locating you…</p>";
+    myLabel.style.display = 'block';
+    subBtn.style.display = 'block';
+    parkSearch.style.display = 'block';
+    navigator.geolocation.getCurrentPosition(success, error);
+  });
 
   document.getElementById('submit-btn').addEventListener('click', function(event){
   event.preventDefault();
