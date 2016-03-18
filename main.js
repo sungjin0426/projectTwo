213d6aa859cdfb4dd09ed437c06eead2ff79588b
sungjin0426 <sjlbest0426@gmail.com>
2016-03-18 14:55:08 -0400

    project time

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
-window.onload = function() {
+  window.onload = function() {
   console.log("main.js is fully loaded");
   //initial grab
+    //by id
+  var dropdown = document.querySelector('#drop');
   var mapMe = document.querySelector('#map-me');
-  var findMeBtn = document.querySelector('.find-me-btn');
-  var foundMe = document.querySelector('.current-location');
-  var latLong = document.querySelector('.lat-long');
-  var myLabel = document.querySelector('.my-label');
   var parkSearch = document.querySelector('#park-search');
   var subBtn = document.querySelector('#submit-btn');
-  var dropdown = document.querySelector('#drop');
+  var searchBtn = document.querySelector('#search-btn')
+    //by class
   var clickMe = document.querySelector('.click-me');
+  var foundMe = document.querySelector('.current-location');
+  var findMeBtn = document.querySelector('.find-me-btn');
+  var latLong = document.querySelector('.lat-long');
+  var myLabel = document.querySelector('.my-label');
 
   var infoContainer = document.getElementById('info-container');
   infoContainer.style.display = 'none';
   myLabel.style.display = 'none';
-  subBtn.style.display = 'none';
   parkSearch.style.display = 'none';
+  searchBtn.style.display = 'none';
+  subBtn.style.display = 'none';
 
   //City's public recyclbin api from nyc opendata
   var baseQuery = 'https://data.cityofnewyork.us/resource/sxx4-xhzg.json';
@@ -29,11 +33,11 @@ window.onload = function() {
     mapMe.innerHTML = "<p>Geolocation is not supported by your browser</p>";
     console.log("Turn on your locator");
   }
+
   //if geolocation is successfully loaded,
   function success(position) {
     myLabel.style.display = 'block';
     subBtn.style.display = 'block';
-    parkSearch.style.display = 'block';
     var myLat  = position.coords.latitude;
     var myLong = position.coords.longitude;
     mapMe.innerHTML = '<p>Latitude is: ' + myLat + '° <br>Longitude is: ' + myLong + '°</p>';
@@ -64,12 +68,11 @@ window.onload = function() {
     navigator.geolocation.getCurrentPosition(success, error);
   });
 
-
-
   //dropdown for borough
-
   document.getElementById('submit-btn').addEventListener('click', function(event){
-  event.preventDefault();
+  parkSearch.style.display = 'block';
+  searchBtn.style.display = 'block';
+  subBtn.remove();
 
   var boroughDrop = document.querySelector('.boroughNYC:checked');
 
@@ -79,14 +82,29 @@ window.onload = function() {
       url: findBoroughQuery,
     }).done(function(findBoroughQueryresponse){
       console.log(findBoroughQueryresponse);
-      for (var i = 0; i < findBoroughQueryresponse.length; i++) {
-      }
     }).fail(function(findBoroughQueryresponse){
       console.log("fail");
     }).always(function(findBoroughQueryresponse){
       console.log("always running");
     }); // end query
+  });// end submit click fxn
 
-  });// end click fxn
+  //seaching parks' recycle bin in NYC through the borough
+  document.getElementById('search-btn').addEventListener('click', function(event){
+
+    var userChosenPark = parkSearch.value.toLowerCase().replace(" ", "%20");
+
+    var findParkQuery = baseQuery + "?park_site_name=" + userChosenPark;
+
+    $.ajax({
+      url: findParkQuery,
+    }).done(function(findParkQueryresponse){
+      console.log(findParkQueryresponse);
+    }).fail(function(findParkQueryresponse){
+      console.log("fail");
+    }).always(function(findParkQueryresponse){
+      console.log("always running");
+    }); // end query
+  });// end serch click fxn
 
 };
